f37f59cc9788a062e13a60400212b85b9d1115ec
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-02-20 11:56:10 +0100

    basic site and nav structure

--- lib/default.rb
+++ lib/default.rb
@@ -2,7 +2,7 @@ include Nanoc3::Helpers::XMLSitemap
 # include Nanoc3::Helpers::Tagging
 # include Nanoc3::Helpers::Rendering
 # include Nanoc3::Helpers::Blogging
-# include Nanoc3::Helpers::LinkTo
+include Nanoc3::Helpers::LinkTo
 
 require 'fileutils'
 
