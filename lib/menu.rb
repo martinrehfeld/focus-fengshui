5c6f8d06bbf1f779d7c6e66566c1d5e835af9a9b
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-02-20 14:26:06 +0100

    automatically include <h2>'s in content as level2 menu items

---
+++ lib/menu.rb
@@ -0,0 +1,29 @@
+require 'kramdown'
+require 'nokogiri'
+
+def menu_item(text, target, attributes={})
+  # Find item
+  item = target.is_a?(String) ? items.detect { |item| item.path == target } : target
+  raise "Item #{target.inspect} could not be found!" unless item
+
+  # set default :class
+  if attributes[:class]
+    attributes[:class] << ' '
+  else
+    attributes[:class] = ''
+  end
+  attributes[:class] << 'level1'
+
+  # parse content and find <h2> tags
+  unless item[:extension] =~ /(.md|.markdown)\Z/
+    raise "Only markdown is supported as source format for menu item pages (got: #{item[:extension].inspect})" unless item[:extension] =~ /(.md|.markdown)\Z/
+  end
+  content = Kramdown::Document.new(item.raw_content).to_html
+  doc = Nokogiri::HTML.parse(content)
+  level2_links = []
+  doc.xpath('//h2').each do |subheading|
+    level2_links << link_to(subheading.content, "#{item.path}##{subheading['id']}", :class => 'level2')
+  end
+
+  link_to(text, item, attributes) + level2_links.join
+end
