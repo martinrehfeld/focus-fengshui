d22d1ad4919180b37683cda9887cfc8c1621dc87
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-02-27 14:02:35 +0100

    add toplinks to .content h2

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -1,8 +1,14 @@
-var focusFengShui = {
+({
   init: function() {
     // support HTML5 elements in older browsers
-    'article aside footer header nav section time'.replace(/\w+/g,function(n){document.createElement(n)});
-  }
-};
+    'article aside footer header nav section time'.replace(/\w+/g,function (n) {
+      document.createElement(n);
+    });
 
-focusFengShui.init();
+    $(this.addToplinks);
+  },
+
+  addToplinks: function () {
+    $('#main .content h2').append('<a class="toplink" href="#container">↑ Seitenanfang</a>');
+  }
+}).init();
