716cda6dbae353720f4eda25026fd743de5b51be
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-02-27 21:16:22 +0100

    structure menu by nesting
    * do not automatically provide anchor-links to
      <h2>'s in the content

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,33 +1,20 @@
-require 'kramdown'
-require 'nokogiri'
-
 def menu_item(text, target, attributes={})
   # Find item
   item = target.is_a?(String) ? items.detect { |item| item.path == target } : target
   raise "Item #{target.inspect} could not be found!" unless item
 
-  # set default :class
+  # set level according to path nesting
   if attributes[:class]
     attributes[:class] << ' '
   else
     attributes[:class] = ''
   end
-  attributes[:class] << 'level1'
+  attributes[:class] << "level#{item.path.count('/') - 1}"
 
+  # mark current item
   if item == @item
     attributes[:class] << ' current'
   end
 
-  # parse content and find <h2> tags
-  unless item[:extension] =~ /(.md|.markdown)\Z/
-    raise "Only markdown is supported as source format for menu item pages (got: #{item[:extension].inspect})" unless item[:extension] =~ /(.md|.markdown)\Z/
-  end
-  content = Kramdown::Document.new(item.raw_content).to_html
-  doc = Nokogiri::HTML.parse(content)
-  level2_links = []
-  doc.xpath('//h2').each do |subheading|
-    level2_links << link_to(subheading.content, "#{item.path}##{subheading['id']}", :class => 'level2')
-  end
-
-  link_to(text, item, attributes) + level2_links.join
+  link_to(text, item, attributes)
 end
