3d841fb39ffaf08750c058c11734bd67e939b7e7
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-02-28 19:55:49 +0100

    extract menu into layout partial

--- lib/default.rb
+++ lib/default.rb
@@ -1,9 +1,9 @@
 include Nanoc3::Helpers::XMLSitemap
-# include Nanoc3::Helpers::Tagging
-# include Nanoc3::Helpers::Rendering
-# include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::Capturing
 include Nanoc3::Helpers::LinkTo
+# include Nanoc3::Helpers::Blogging
+# include Nanoc3::Helpers::Tagging
 
 require 'fileutils'
 
