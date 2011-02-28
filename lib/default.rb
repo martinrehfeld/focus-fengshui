ea1fe12166438f25918bf7589584aa6816b6cfe0
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-02-28 18:33:37 +0100

    simplify route_path helper

--- lib/default.rb
+++ lib/default.rb
@@ -7,17 +7,12 @@ include Nanoc3::Helpers::LinkTo
 
 require 'fileutils'
 
-# Taken from: http://github.com/mgutz/nanoc3_blog/blob/master/lib/helpers.rb
-#
-# Hyphens are converted to sub-directories in the output folder.
+# Adapted from: http://github.com/mgutz/nanoc3_blog/blob/master/lib/helpers.rb
 #
 # If a file has two extensions like Rails naming conventions, then the first extension
 # is used as part of the output file.
 #
 #   sitemap.xml.erb # => sitemap.xml
-#
-# If the output file does not end with an .html extension, item[:layout] is set to 'none'
-# bypassing the use of layouts.
 #
 def route_path(item)
   # in-memory items have no file
@@ -36,10 +31,6 @@ def route_path(item)
     outext = '.html'
   end
   url.gsub!(extname, outext)
-
-  if url.include?('-')
-    url = url.split('-').join('/')  # /2010/01/01-some_title.html -> /2010/01/01/some_title.html
-  end
 
   url
 end
